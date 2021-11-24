95ed8485b76f1acf688b53e19b0156aa402c96f3
Dan Snow <danjsnow@outlook.com>
2021-11-24 18:24:30 +0000

    Made name search insensitive

--- general_functions/arg_flexible_name_search.R
+++ general_functions/arg_flexible_name_search.R
@@ -1,9 +1,11 @@
 ns <- function(arg_1, arg_2){
   
   if(exists(substitute(arg_1))){
-    result <- return(names(arg_1)[(grep(substitute(arg_2), names(arg_1)))])
+    result <- return(names(arg_1)[(grep(substitute(arg_2), names(arg_1), 
+                                        ignore.case = TRUE))])
   } else if(exists(substitute(arg_2))){
-  result <- return(names(arg_2)[(grep(substitute(arg_1), names(arg_2)))])
+  result <- return(names(arg_2)[(grep(substitute(arg_1), names(arg_2),
+                                      ignore.case = TRUE))])
   }
   else{
     result <- "Please supply a dataframe as one of the arguments"
